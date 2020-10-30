212168ebc5c8e21debf2df833af6ffc668dee504
davidroid <davide.aliprandi@gmail.com>
2020-10-31 00:17:42 +0100

    Add libsdbsdk.so to the pypi package

--- build_pypi_pkg.sh
+++ build_pypi_pkg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # if not already installed
 # pip3 install sdist wheel twine
-python3 setup.py sdist bdist_wheel  &&  python3 -m twine upload --skip-existing --repository  pypi  dist/*
+python3 setup.py sdist bdist_wheel  &&  python3 -m twine upload --repository  pypi  dist/*
